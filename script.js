89f328be7aa4d59d85dcf889a7dff922c618e5cd
anugrahsaputra <143504583+anugrahsaputra@users.noreply.github.com>
2025-01-17 22:44:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -60,3 +60,14 @@ const observer = new IntersectionObserver((entries, observer) => {
 elements.forEach(element => {
     observer.observe(element);
 });
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const menuIcon = document.querySelector('.menu-icon');
+        const navMenu = document.querySelector('nav ul');
+
+        menuIcon.addEventListener('click', function () {
+            navMenu.classList.toggle('show');
+        });
+    });
+</script>
