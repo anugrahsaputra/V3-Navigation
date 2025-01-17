ff6fb2390edcda546ec830ce1637e67b0c2fad36
anugrahsaputra <143504583+anugrahsaputra@users.noreply.github.com>
2025-01-17 18:15:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ window.addEventListener('scroll', function() {
   });
 });
 
+const sections = document.querySelectorAll('.fade-in');
+const options = { threshold: 0.5 };
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('appear');
+            observer.unobserve(entry.target);
+        }
+    });
+}, options);
+
+sections.forEach(section => observer.observe(section));
